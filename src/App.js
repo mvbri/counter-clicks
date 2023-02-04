2e3b64479b8375bf1e918f0e84d8a97209304853
maria <mvbri10@gmail.com>
2023-02-04 18:09:29 -0400

    Preparamos proyecto, importamos logo y se agregaron estilos basicos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import mainLogo from './img/logo.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='container-logo'>
+				<img className='main-logo' alt='Logo principal' src={mainLogo} />
+			</div>
+			<div className='container-counter'></div>
+		</div>
+	);
 }
 
 export default App;
